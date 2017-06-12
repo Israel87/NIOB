a90c6ca7d1e77011ec828b5624364a05f223c0c4
innaji <nnajiisrael@gmail.com>
2017-06-12 19:00:08 +0100

    Backend validation, Date/Time trimming, email/phone validation

--- NIOB/Util/ConnectionManager.cs
+++ NIOB/Util/ConnectionManager.cs
@@ -54,7 +54,9 @@ namespace NIOB.Util
             bool response;
 
             //format date
-
+            string DOB = Convert.ToDateTime(user.biodata.DOB).ToString("dd-MMM-yyyy");
+            string StartDate = Convert.ToDateTime(user.emp_info.StartDate).ToString("dd-MMM-yyyy");
+            string DateJoined = Convert.ToDateTime(user.association.Datejoined).ToString("dd-MMM-yyyy");
 
 
             try
@@ -68,7 +70,7 @@ namespace NIOB.Util
                 cmd.Parameters.Add(new OracleParameter("V_TITLE", OracleDbType.Varchar2, user.biodata.Title, ParameterDirection.Input));
                 cmd.Parameters.Add(new OracleParameter("V_FIRSTNAME", OracleDbType.Varchar2, user.biodata.Firstname, ParameterDirection.Input));
                 cmd.Parameters.Add(new OracleParameter("V_SURNAME", OracleDbType.Varchar2, user.biodata.Surname, ParameterDirection.Input));
-                //cmd.Parameters.Add(new OracleParameter("V_DOB", OracleDbType.Date, user.biodata.DOB, ParameterDirection.Input));
+                cmd.Parameters.Add(new OracleParameter("V_DOB", OracleDbType.Date, DOB, ParameterDirection.Input));
                 cmd.Parameters.Add(new OracleParameter("V_GENDER", OracleDbType.Varchar2, user.biodata.Gender, ParameterDirection.Input));
                 cmd.Parameters.Add(new OracleParameter("V_PHONENUMBER", OracleDbType.Varchar2, user.biodata.PhoneNumber, ParameterDirection.Input));
                 cmd.Parameters.Add(new OracleParameter("V_STATE", OracleDbType.Varchar2, user.biodata.State, ParameterDirection.Input));
@@ -89,17 +91,17 @@ namespace NIOB.Util
                 cmd.Parameters.Add(new OracleParameter("V_EMPLOYER", OracleDbType.Varchar2, user.emp_info.Employer, ParameterDirection.Input));
                 cmd.Parameters.Add(new OracleParameter("V_EMPLOYERADDRESS", OracleDbType.Varchar2, user.emp_info.Address, ParameterDirection.Input));
                 cmd.Parameters.Add(new OracleParameter("V_SCOPE", OracleDbType.Varchar2, user.emp_info.Scope, ParameterDirection.Input));
-               // cmd.Parameters.Add(new OracleParameter("V_STARTDATE", OracleDbType.Date, null, ParameterDirection.Input));
+                cmd.Parameters.Add(new OracleParameter("V_STARTDATE", OracleDbType.Date, StartDate, ParameterDirection.Input));
                 cmd.Parameters.Add(new OracleParameter("V_EMPSTATUS", OracleDbType.Varchar2, user.emp_info.Status, ParameterDirection.Input));
                 //tbl_user info
                 cmd.Parameters.Add(new OracleParameter("V_USERNAME", OracleDbType.Varchar2, user.tbl_user.Username, ParameterDirection.Input));
                 cmd.Parameters.Add(new OracleParameter("V_PASSWORD", OracleDbType.Varchar2, user.tbl_user.Password, ParameterDirection.Input));
                 cmd.Parameters.Add(new OracleParameter("V_EMAIL", OracleDbType.Varchar2, user.tbl_user.Email, ParameterDirection.Input));
                 cmd.Parameters.Add(new OracleParameter("V_USERTYPE", OracleDbType.Varchar2, user.tbl_user.UserType, ParameterDirection.Input));
-                //assocaition info
+                //association info
                 cmd.Parameters.Add(new OracleParameter("V_ASSOCIATIONNAME", OracleDbType.Varchar2, user.association.Name, ParameterDirection.Input));
                 cmd.Parameters.Add(new OracleParameter("V_POSITION", OracleDbType.Varchar2, user.association.PositionHeld, ParameterDirection.Input));
-                //cmd.Parameters.Add(new OracleParameter("V_DATEJOINED", OracleDbType.Date, null, ParameterDirection.Input));
+                cmd.Parameters.Add(new OracleParameter("V_DATEJOINED", OracleDbType.Date, DateJoined, ParameterDirection.Input));
                 //doc info
                 cmd.Parameters.Add(new OracleParameter("V_DOCTYPE", OracleDbType.Varchar2, user.doc_info.DocumentType, ParameterDirection.Input));
                 cmd.Parameters.Add(new OracleParameter("V_DOCURL", OracleDbType.Varchar2, user.doc_info.DocumentUrl, ParameterDirection.Input));
